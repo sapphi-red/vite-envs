ab90fb3979b1eeada7248f497e1ee4c4db338ac3
sapphi-red <49056869+sapphi-red@users.noreply.github.com>
2024-02-16 12:10:08 +0900

    refactor: remove cloudflare-pages-emulate

--- examples/cloudflare-pages-emulate/_worker.ts
+++
@@ -1,6 +0,0 @@
-export { default } from './worker/index.js'
-
-// stop auto reload
-if (import.meta.hot) {
-  import.meta.hot.accept()
-}
--- examples/cloudflare-pages-emulate/worker/types.ts
+++
@@ -1,8 +0,0 @@
-import type { KVNamespace } from '@cloudflare/workers-types'
-
-export type Env = {
-  ASSETS: {
-    fetch: typeof fetch
-  }
-  FOO_KV: KVNamespace
-}
